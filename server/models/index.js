e7c4c28fc6f57e3c7c695e8d45333cefb24415cc
Marina <marinajalonzo1@gmail.com>
2024-04-01 17:58:01 -0500

    Book model added.

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Thought = require('./Thought');
+const Book = require('./Book');
 
-module.exports = { User, Thought };
+module.exports = { User, Thought, Book };
